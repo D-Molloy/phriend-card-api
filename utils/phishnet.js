60282ee212b022b6e4d39370352153154e3d1eb3
Denis Molloy <denis.molloy@gmail.com>
2020-03-23 18:23:38 -0700

    successfully parsing show data

--- utils/phishnet.js
+++ utils/phishnet.js
@@ -1,40 +1,34 @@
-const axios = require("axios");
-const cheerio = require("cheerio")
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const parseShowHtml = showHtml => {
 
 
-const parseShowHtml =(showHtml)=>{
-  console.log("PARSING DATA")
-  console.log("showHtml -",showHtml)
-  // const setlistEl = document.createElement("body")
+  let setlistInfo = {
+    songCount: 0
+  };
 
-  // setlistEl.innerHTML = setlistHtml
+  let setCount = 0;
 
-  const detailsAr = []
-  // const detailsAr = setlistEl.querySelectorAll("[class^='set']")
+  const $ = cheerio.load(showHtml);
 
-  let showDetails = {
-    songCount:0
-  }
-  let currentSetAr = []
-  let setCount = 0
-  
-
-  for(item of detailsAr){
-    if(item.classList.contains("set-label")){
-      currentSetAr = []
-      setCount++
-      showDetails[`set${setCount}`] = currentSetAr
+  $('[class^="set"]').each(function(_, element) {
+    const songName = $(element).text();
+    const className = $(element).attr('class');
+    if (className === 'set-label') {
+      setCount++;
+      setlistInfo[`set${setCount}`] = [];
     }
 
-    if(item.classList.contains("setlist-song")){
-      showDetails[`set${setCount}`].push(item.textContent)
-      showDetails.songCount++
+    if (className === 'setlist-song') {
+      setlistInfo[`set${setCount}`].push(songName);
+      setlistInfo.songCount++;
     }
-}
-return showDetails
-}
+  });
 
+  return setlistInfo;
+};
 
-module.exports={
+module.exports = {
   parseShowHtml
-}
+};
